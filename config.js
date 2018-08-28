6a17da780f6e31e515924a419fc5972c3f04e13f
meepobrother <1037483576@qq.com>
2018-08-28 22:55:58 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+System.config({
+  baseURL: "./",
+  map: {
+    "@angular/core": "libs/@angular/core.umd.min.js",
+    "@angular/common": "libs/@angular/common.umd.min.js",
+    "@angular/common/http": "libs/@angular/common-http.umd.min.js",
+    "@angular/forms": "libs/@angular/forms.umd.min.js",
+    "@angular/router": "libs/@angular/router.umd.min.js",
+    "@angular/platform-browser": "libs/@angular/platform-browser.umd.min.js",
+    "@angular/platform-browser-dynamic": "libs/@angular/platform-browser-dynamic.umd.min.js",
+    "rxjs": "libs/rxjs.umd.min.js",
+    "rxjs/operators": "libs/rxjs.umd.min.js",
+    "jquery": "./libs/jquery.min.js"
+  }
+});
