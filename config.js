6bde63a4177741c0e0afd01eb37e1b630dc90d51
meepobrother <1037483576@qq.com>
2018-08-28 23:06:48 +0800

    u

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 System.config({
   baseURL: "./",
   map: {
-    "@angular/core": "libs/@angular/core.umd.min.js",
-    "@angular/common": "libs/@angular/common.umd.min.js",
-    "@angular/common/http": "libs/@angular/common-http.umd.min.js",
-    "@angular/forms": "libs/@angular/forms.umd.min.js",
-    "@angular/router": "libs/@angular/router.umd.min.js",
-    "@angular/platform-browser": "libs/@angular/platform-browser.umd.min.js",
-    "@angular/platform-browser-dynamic": "libs/@angular/platform-browser-dynamic.umd.min.js",
-    "rxjs": "libs/rxjs.umd.min.js",
-    "rxjs/operators": "libs/rxjs.umd.min.js",
-    "jquery": "./libs/jquery.min.js",
-    "@angular/elements": "./libs/@angular/elements.umd.min.js",
-    "@angular/platform-webworker": "./libs/@angular/platform-webworker.umd.min.js",
-    "@angular/platform-webworker-dynamic": "./libs/@angular/platform-webworker-dynamic.umd.min.js",
-    "@angular/service-worker": "./libs/@angular/service-worker.umd.min.js"
+    "@angular/core": "assets/library/@angular/core.umd.min.js",
+    "@angular/common": "assets/library/@angular/common.umd.min.js",
+    "@angular/common/http": "assets/library/@angular/common-http.umd.min.js",
+    "@angular/forms": "assets/library/@angular/forms.umd.min.js",
+    "@angular/router": "assets/library/@angular/router.umd.min.js",
+    "@angular/platform-browser": "assets/library/@angular/platform-browser.umd.min.js",
+    "@angular/platform-browser-dynamic": "assets/library/@angular/platform-browser-dynamic.umd.min.js",
+    "rxjs": "assets/library/rxjs.umd.min.js",
+    "rxjs/operators": "assets/library/rxjs.umd.min.js",
+    "jquery": "assets/library/jquery.min.js",
+    "@angular/elements": "assets/library/@angular/elements.umd.min.js",
+    "@angular/platform-webworker": "assets/library/@angular/platform-webworker.umd.min.js",
+    "@angular/platform-webworker-dynamic": "assets/library/@angular/platform-webworker-dynamic.umd.min.js",
+    "@angular/service-worker": "assets/library/@angular/service-worker.umd.min.js"
   }
 });
